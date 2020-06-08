1acbcd9aaa633d4a058fb742ea8d5e2b1759f757
Yuliya <blondinekatze@gmail.com>
2020-06-08 13:38:46 +0700

    common

--- src/assets/js/vendor.js
+++ src/assets/js/vendor.js
@@ -1,7 +1,6 @@
 //=require jquery/dist/jquery.js
 //=require @fancyapps/fancybox/dist/jquery.fancybox.min.js
 //=require inputmask/dist/jquery.inputmask.bundle.js
-//=require ouibounce/build/ouibounce.min.js
 
 //=require vanilla-lazyload/dist/lazyload.js
 
